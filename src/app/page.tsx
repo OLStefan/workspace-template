8dee67aa7d2dc73b991c878a1720364720e4e631
Stefan Niewerth <stefan.niewerth@gmx.net>
2024-04-26 22:15:43 +0200

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import type { ReactNode } from 'react';
+import './globals.css';
 import styles from './page.module.css';
 
 export default function Page(): ReactNode {
 	return (
 		<div className={styles.root}>
-			<span>Hello World</span>
+			<span className="text-3xl font-bold underline">Hello World</span>
 		</div>
 	);
 }
